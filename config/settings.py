620a4456da69f8b3cfcd7295e24b64df0e565255
Purna Chandra Rao <dhanekula591@gmail.com>
2025-12-26 12:08:29 +0530

    Fix: Add EMAIL_TIMEOUT and ensure CORS middleware is properly ordered

--- config/settings.py
+++ config/settings.py
@@ -73,9 +73,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add whitenoise for static files
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # MUST be before CommonMiddleware
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -146,6 +146,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 5  # Timeout after 5 seconds to prevent hanging
 
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='peelojuice0@gmail.com')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='effa yadv dzyg hwcw')
